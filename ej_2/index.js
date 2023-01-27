ad9d9ab36bdea2107a4e1d548e71464ca3862ac1
barbaraforba <122522708+barbaraforba@users.noreply.github.com>
2023-01-27 10:40:23 +0100

    ej2

---
+++ ej_2/index.js
@@ -0,0 +1,8 @@
+const cumple = {
+    dia:8,
+    mes:'Noviembre'
+}
+console.log('Mi cumpleaños es el día '+ cumple.dia + ' de ' + cumple.mes);
+
+let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+console.log(meses[11]);
