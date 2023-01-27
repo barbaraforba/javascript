755be57fc531cc9fc743a5c327ff68f880c523ca
barbaraforba <122522708+barbaraforba@users.noreply.github.com>
2023-01-27 10:00:12 +0100

    ejercicio1

---
+++ ej_1/index.js
@@ -0,0 +1,10 @@
+
+// base 16*23
+// altura 13
+
+const volumen = 16*23*13;
+console.log('volumen',volumen + 'cm3');
+const area_lat = 23*13;
+const area_base = 16*23;
+const area_tot= area_lat + 2*area_base;
+console.log('area_tot', area_tot + 'cm2');
