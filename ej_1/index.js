99ecbf0d742c897b1a628bfc615ff70c7a230bc5
barbaraforba <122522708+barbaraforba@users.noreply.github.com>
2023-01-27 10:04:11 +0100

    Update index.js

--- ej_1/index.js
+++ ej_1/index.js
@@ -8,3 +8,14 @@ const area_lat = 23*13;
 const area_base = 16*23;
 const area_tot= area_lat + 2*area_base;
 console.log('area_tot', area_tot + 'cm2');
+
+
+const base = 23;
+const altura = 13;
+const ancho = 16;
+const volumen1 = base*altura*ancho;
+console.log('volumen1', volumen1);
+
+const area1= base*altura + 2*base*ancho;
+console.log('area1', area1);
+
